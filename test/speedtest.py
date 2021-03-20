669d904abf49f6e9997975c89651c8c3ed208594
Nico Schlömer <nico.schloemer@gmail.com>
2021-03-20 09:22:54 +0100

    add speedtest

--- test/speedtest.py
+++ test/speedtest.py
@@ -2,28 +2,40 @@ import numpy as np
 import npx
 import perfplot
 
+np.random.seed(0)
+
+m = 100
+
 
 def setup(n):
-    a = np.random.rand(n)
-    idx = np.random.randint(0, n, n)
+    idx = np.random.randint(0, m, n)
     b = np.random.rand(n)
-    return a, idx, b
+    return idx, b
 
 
 def np_add_at(data):
-    a, idx, b = data
+    a = np.zeros(m)
+    idx, b = data
     np.add.at(a, idx, b)
     return a
 
 
 def npx_add_at(data):
-    a, idx, b = data
+    a = np.zeros(m)
+    idx, b = data
     npx.add_at(a, idx, b)
     return a
 
 
-perfplot.show(
+def npx_sum_at(data):
+    idx, b = data
+    out = npx.sum_at(b, idx, minlength=m)
+    return out
+
+
+b = perfplot.bench(
     setup=setup,
-    kernels=[np_add_at, npx_add_at],
-    n_range=[2 ** k for k in range(23)]
+    kernels=[np_add_at, npx_add_at, npx_sum_at],
+    n_range=[2 ** k for k in range(23)],
 )
+b.save("perf-add-at.svg")
