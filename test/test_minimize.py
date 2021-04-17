545231b35f4275d694a5dda894e125ccae7d00ad
Nico Schlömer <nico.schloemer@gmail.com>
2021-04-17 16:50:05 +0200

    add another minimize test

--- test/test_minimize.py
+++ test/test_minimize.py
@@ -3,12 +3,23 @@ import numpy as np
 import npx
 
 
-def test_1d():
+def test_0d():
     def f(x):
         return (x ** 2 - 2) ** 2
 
     x0 = 1.5
     out = npx.minimize(f, x0)
+
+    assert out.x.shape == np.asarray(x0).shape
+    assert np.asarray(out.fun).shape == ()
+
+
+def test_2d():
+    def f(x):
+        return (np.sum(x ** 2) - 2) ** 2
+
+    x0 = np.ones((4, 3), dtype=float)
+    out = npx.minimize(f, x0, method="Powell")
 
     assert out.x.shape == np.asarray(x0).shape
     assert np.asarray(out.fun).shape == ()
