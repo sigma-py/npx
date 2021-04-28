27d2e1c0db57fa6e4e13043eeefc401b14269487
Nico Schlömer <nico.schloemer@gmail.com>
2021-04-28 18:49:21 +0200

    fix init

--- npx/__init__.py
+++ npx/__init__.py
@@ -1,9 +1,6 @@
 from .__about__ import __version__
 from ._isin import isin_rows
-from ._krylov import bicg, bicgstab, cg, cgs, gmres, minres, qmr
 from ._main import add_at, dot, solve, subtract_at, sum_at, unique_rows
-from ._minimize import minimize
-from ._nonlinear import bisect, regula_falsi
 
 __all__ = [
     "__version__",
@@ -14,16 +11,4 @@ __all__ = [
     "subtract_at",
     "unique_rows",
     "isin_rows",
-    #
-    "bicg",
-    "bicgstab",
-    "cg",
-    "cgs",
-    "qmr",
-    "gmres",
-    "minres",
-    #
-    "minimize",
-    "bisect",
-    "regula_falsi",
 ]
