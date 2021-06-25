c019a461a32a3c862ab48d30a9bd188e594852bc
Nico Schlömer <nico.schloemer@gmail.com>
2021-06-25 19:04:31 +0200

    remove importlib dependency

--- src/npx/__init__.py
+++ src/npx/__init__.py
@@ -1,10 +1,8 @@
-from .__about__ import __version__
 from ._isin import isin_rows
 from ._main import add_at, dot, solve, subtract_at, sum_at, unique_rows
 from ._mean import mean
 
 __all__ = [
-    "__version__",
     "dot",
     "solve",
     "sum_at",
