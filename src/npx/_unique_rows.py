49c196007efb94bb2b724da62a7fada009b1cfbd
Nico Schlömer <nico.schloemer@gmail.com>
2021-08-18 23:50:34 +0200

    add return_index

--- src/npx/_unique_rows.py
+++ src/npx/_unique_rows.py
@@ -5,7 +5,10 @@ from numpy.typing import ArrayLike
 
 
 def unique_rows(
-    a: ArrayLike, return_inverse: bool = False, return_counts: bool = False
+    a: ArrayLike,
+    return_index: bool = False,
+    return_inverse: bool = False,
+    return_counts: bool = False,
 ) -> Union[np.ndarray, Tuple]:
     # The numpy alternative `np.unique(a, axis=0)` is slow; cf.
     # <https://github.com/numpy/numpy/issues/11136>.
@@ -15,7 +18,12 @@ def unique_rows(
     a = a.reshape(a.shape[0], np.prod(a.shape[1:], dtype=int))
 
     b = np.ascontiguousarray(a).view(np.dtype((np.void, a.dtype.itemsize * a.shape[1])))
-    out = np.unique(b, return_inverse=return_inverse, return_counts=return_counts)
+    out = np.unique(
+        b,
+        return_index=return_index,
+        return_inverse=return_inverse,
+        return_counts=return_counts,
+    )
     # out[0] are the sorted, unique rows
     if isinstance(out, tuple):
         out = (out[0].view(a.dtype).reshape(out[0].shape[0], *a_shape[1:]), *out[1:])
