c81b1a029d43e63b9c3542f298b8c8d9da1da2dc
Nico Schlömer <nico.schloemer@gmail.com>
2021-11-17 15:28:14 +0100

    add float unique()

--- src/npx/_unique.py
+++ src/npx/_unique.py
@@ -1,7 +1,36 @@
-from typing import Tuple, Union
+from __future__ import annotations
+
+from typing import Callable
 
 import numpy as np
 from numpy.typing import ArrayLike
+
+
+def _unique_tol(
+    unique_fun: Callable, a: ArrayLike, tol: float, **kwargs
+) -> np.ndarray | tuple[np.ndarray, ...]:
+    a = np.asarray(a)
+    aint = (a / tol).astype(int)
+
+    return_index = kwargs.pop("return_index", False)
+
+    _, idx, *out = unique_fun(aint, return_index=True, **kwargs)
+    unique_a = a[idx]
+
+    if return_index:
+        out = [idx, *out]
+
+    return unique_a, *out
+
+
+def unique(
+    a: ArrayLike, tol: float = 0.0, **kwargs
+) -> np.ndarray | tuple[np.ndarray, ...]:
+    assert tol >= 0.0
+    if tol > 0.0:
+        return _unique_tol(np.unique, a, tol, **kwargs)
+
+    return np.unique(a, **kwargs)
 
 
 def unique_rows(
@@ -9,7 +38,7 @@ def unique_rows(
     return_index: bool = False,
     return_inverse: bool = False,
     return_counts: bool = False,
-) -> Union[np.ndarray, Tuple]:
+) -> np.ndarray | tuple[np.ndarray, ...]:
     # The numpy alternative `np.unique(a, axis=0)` is slow; cf.
     # <https://github.com/numpy/numpy/issues/11136>.
     a = np.asarray(a)
