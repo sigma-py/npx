8569dc709c92c560aa13735dd72ac2622ca39888
Nico Schlömer <nico.schloemer@gmail.com>
2026-04-30 16:32:40 +0200

    fix tests, deprecation messages

--- src/npx/_main.py
+++ src/npx/_main.py
@@ -30,6 +30,7 @@ def solve(A: np.ndarray, x: np.ndarray) -> np.ndarray:
     x = np.asarray(x)
     return np.linalg.solve(A, x.reshape(x.shape[0], -1)).reshape(x.shape)
 
+
 @deprecated("np.add.at is now equally fast. Use that.")
 def sum_at(a: ArrayLike, indices: ArrayLike, minlength: int):
     """Sums up values `a` with `indices` into an output array of at least
@@ -44,6 +45,10 @@ def sum_at(a: ArrayLike, indices: ArrayLike, minlength: int):
     `indices` may have arbitrary shape, too, but then `a` has to start out the
     same. (Those dimensions are flattened out in the computation.)
     """
+    return _sum_at(a, indices, minlength)
+
+
+def _sum_at(a: ArrayLike, indices: ArrayLike, minlength: int):
     a = np.asarray(a)
     indices = np.asarray(indices)
 
@@ -76,16 +81,20 @@ def sum_at(a: ArrayLike, indices: ArrayLike, minlength: int):
 
 @deprecated("np.add.at is now equally fast. Use that.")
 def add_at(a: ArrayLike, indices: ArrayLike, b: ArrayLike):
+    return _add_at(a, indices, b)
+
+
+def _add_at(a: ArrayLike, indices: ArrayLike, b: ArrayLike):
     a = np.asarray(a)
     indices = np.asarray(indices)
     b = np.asarray(b)
 
     m = len(indices.shape)
     assert a.shape[1:] == b.shape[m:]
-    a += sum_at(b, indices, a.shape[0])
+    a += _sum_at(b, indices, a.shape[0])
 
 
 @deprecated("np.subtract.at is now equally fast. Use that.")
 def subtract_at(a: ArrayLike, indices: ArrayLike, b: ArrayLike):
     b = np.asarray(b)
-    add_at(a, indices, -b)
+    _add_at(a, indices, -b)
--- tests/test_at.py
+++ tests/test_at.py
@@ -8,7 +8,7 @@ def test_sum_at():
     a = [1.0, 2.0, 3.0]
     idx = [0, 1, 0]
 
-    with pytest.warns(DeprecationWarning):
+    with pytest.warns(DeprecationWarning, match=r"sum_at\(\) is deprecated.*"):
         out = npx.sum_at(a, idx, minlength=4)
 
     tol = 1.0e-13
@@ -21,7 +21,7 @@ def test_add_at():
     idx = [0, 1, 0]
     out = np.zeros(2)
 
-    with pytest.warns(DeprecationWarning):
+    with pytest.warns(DeprecationWarning, match=r"add_at\(\) is deprecated.*"):
         npx.add_at(out, idx, a)
 
     tol = 1.0e-13
@@ -34,7 +34,7 @@ def test_subtract_at():
     idx = [0, 1, 0]
     out = np.ones(2)
 
-    with pytest.warns(DeprecationWarning):
+    with pytest.warns(DeprecationWarning, match=r"subtract_at\(\) is deprecated.*"):
         npx.subtract_at(out, idx, a)
 
     tol = 1.0e-13
